19bd48743d7b580b03c7e94e14f9e584165928fb
Luis Gomes <luis12osg@gmail.com>
2023-09-02 20:16:37 -0300

    Resolução dos desafios de código

---
+++ 02_Resolvendo-Seus-Primeiros-Desafios-de-Código/Deasfios-Python-Organizando-Seus-Ativos.py
@@ -0,0 +1,15 @@
+ativos = []
+
+# Entrada da quantidade de ativos
+quantidadeAtivos = int(input())
+
+# Entrada dos códigos dos ativos
+for _ in range(quantidadeAtivos):
+    codigoAtivo = input()
+    ativos.append(codigoAtivo)
+
+# TODO: Ordenar os ativos em ordem alfabética.
+ativos.sort()
+# TODO: Imprimir a lista de ativos ordenada, conforme a tabela de exemplos.
+for ativo in ativos:
+    print(ativo)
